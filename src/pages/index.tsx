eb5fe039be4ad6dbba15d494051360e13749fc9c
Kevin Smith <ksmithbaylor@gmail.com>
2019-03-16 23:28:21 -0400

    Add layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+import Layout from '~/components/Layout'
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <Header>Hello Gatsby!</Header>
+    <p>Content</p>
+  </Layout>
+)
+
+const Header = styled.div`
+  font-size: 65px;
+`
