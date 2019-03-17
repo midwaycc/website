b40f21fc16c0dc2e39481f41c27c7670a839c06d
Kevin Smith <ksmithbaylor@gmail.com>
2019-03-17 00:05:17 -0400

    Add more to the layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
 import Head from './Head'
+import PageContainer from './PageContainer'
+import Header from './Header'
+import Footer from './Footer'
 
 export default (props: { children: React.ReactNode }) => (
   <>
     <Head />
-    <div style={{ color: 'rebeccapurple' }}>{props.children}</div>
+    <GlobalStyles />
+    <PageContainer>
+      <Header />
+      <main>{props.children}</main>
+      <Footer />
+    </PageContainer>
   </>
 )
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    font-size: 100%;
+  }
+
+  body {
+
+  }
+
+  body, html {
+    margin: 0;
+    padding: 0;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,6 @@ import Layout from '~/components/Layout'
 
 export default () => (
   <Layout>
-    <Header>Hello Gatsby!</Header>
-    <p>Content</p>
+    <p>Home page content</p>
   </Layout>
 )
-
-const Header = styled.div`
-  font-size: 65px;
-`
