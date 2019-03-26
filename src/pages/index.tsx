35fa5b4a849d243902cfa3c7bea2538f7f2ff23d
Kevin Smith <ksmithbaylor@gmail.com>
2019-03-25 21:51:58 -0400

    Remove unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
-import { Link } from 'gatsby'
-import Content from '~/layout/Content'
+import styled from 'styled-components'
 import Section from '~/layout/Section'
 
 export default () => (
