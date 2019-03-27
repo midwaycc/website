dd0af0fd30d89307ba81acbe7eb760c261807a10
Kevin Smith <ksmithbaylor@gmail.com>
2019-03-26 21:26:33 -0400

    Rough start to second section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import Section from '~/layout/Section'
+import HorizontalSection from '~/layout/HorizontalSection'
+import InfoWidget from '~/components/InfoWidget'
 
 const data = {
   title: 'Welcome.',
@@ -16,12 +18,26 @@ export default () => (
       <Arrow css="left: 1em">{'<'}</Arrow>
       <Arrow css="right: 1em">{'>'}</Arrow>
     </Slideshow>
+    <HorizontalSection color="#524637" css="color: white; padding: 5em 0">
+      <InfoWidget title="Sermons">
+        <Rectangle>Listen</Rectangle>
+        <Rectangle>Watch Live</Rectangle>
+      </InfoWidget>
+      <InfoWidget title="Worship Schedule">
+        <div>Bible Study: 9:30 AM</div>
+        <div>Worship Service: 11:00 AM</div>
+      </InfoWidget>
+      <InfoWidget title="Location">
+        <div>3365 Francis Road</div>
+        <div>Alpharetta, Georgia 30004</div>
+      </InfoWidget>
+    </HorizontalSection>
   </>
 )
 
 const Slideshow = styled(Section)`
   width: 100%;
-  height: 500px;
+  height: 30em;
   background-color: #7ba088;
   display: flex;
   flex-direction: column;
@@ -59,4 +75,29 @@ const Arrow = styled.span`
   font-size: 1.5em;
   user-select: none;
   cursor: default;
+`
+
+const Rectangle = styled.button`
+  width: 12em;
+  background-color: #dd9b6b;
+  color: #524637;
+  padding: 1em;
+  font-size: 1.2em;
+  border: none;
+  box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.5);
+  transition: box-shadow 150ms ease, transform 150ms ease;
+
+  :hover {
+    box-shadow: 10px 10px 25px rgba(0, 0, 0, 0.5);
+    transform: translate(-1px, -2px);
+  }
+
+  :active {
+    box-shadow: 3px 3px 15px rgba(0, 0, 0, 0.5);
+    transform: translate(1px, 2px);
+  }
+
+  & + & {
+    margin-top: 1em;
+  }
 `
