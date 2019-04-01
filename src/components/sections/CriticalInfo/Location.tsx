bfdc626fb826d1d653bfd01ae1c012419bc7eb35
Kevin Smith <ksmithbaylor@gmail.com>
2019-03-31 20:55:03 -0400

    Embed Google map of location

--- src/components/sections/CriticalInfo/Location.tsx
+++ src/components/sections/CriticalInfo/Location.tsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import InfoWidget from './InfoWidget'
+import styled from 'styled-components'
+import { useStaticQuery, graphql } from 'gatsby'
 
-export default () => (
-  <InfoWidget title="Location">
-    <div>3365 Francis Road</div>
-    <div>Alpharetta, Georgia 30004</div>
-  </InfoWidget>
-)
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          googleMapsEmbedSrc
+        }
+      }
+    }
+  `)
+
+  return (
+    <InfoWidget title="Location">
+      <Address>
+        3365 Francis Road
+        <br />
+        Alpharetta, Georgia 30004
+      </Address>
+      <Map
+        src={data.site.siteMetadata.googleMapsEmbedSrc}
+        width={250}
+        height={250}
+      />
+    </InfoWidget>
+  )
+}
+
+const Map = styled.iframe`
+  border: 0;
+  margin-top: 2em;
+`
+
+const Address = styled.p`
+  margin: 0;
+  text-align: center;
+`
