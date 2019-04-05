6e459251d8f07c15d3e9d76ac56e6270d1d29ee6
Kevin Smith <ksmithbaylor@gmail.com>
2019-04-05 09:40:27 -0400

    Change 404 message slightly

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,8 +6,8 @@ export default () => (
   <>
     <Section css="height: 100vh; padding-top: 2em">
       <div>
-        Sorry, that page doesn't seem to exist (yet)!{' '}
-        <Link to="/">Go home</Link>?
+        Sorry, that page doesn't seem to exist (yet)! Do you want to&nbsp;
+        <Link to="/">go home</Link>?
       </div>
     </Section>
   </>
