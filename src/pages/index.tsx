cc01c8e16bf47b15fe906116603139c66d5e50ae
Kevin Smith <ksmithbaylor@gmail.com>
2019-04-04 21:25:46 -0400

    Extract title and subtitle to metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,32 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import Slideshow from '~/components/sections/Slideshow'
 import CriticalInfo from '~/components/sections/CriticalInfo'
 
-export default () => (
-  <>
-    <Slideshow color="#7ba088" title={data.title} subtitle={data.subtitle} />
-    <CriticalInfo />
-  </>
-)
-
-const data = {
-  title: 'Welcome!',
-  subtitle:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+type Props = {
+  data: any
 }
+
+export default ({ data }: Props) => {
+  const { title, subtitle } = data.site.siteMetadata.text
+
+  return (
+    <>
+      <Slideshow color="#7ba088" title={title} subtitle={subtitle} />
+      <CriticalInfo />
+    </>
+  )
+}
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        text {
+          title
+          subtitle
+        }
+      }
+    }
+  }
+`
