51453aa4a885861ed21133c66a06830bf04429f8
Kevin Smith <ksmithbaylor@gmail.com>
2019-04-10 21:28:01 -0400

    Add basic switching behavior for responsive nav

--- src/components/header/Navigation.tsx
+++ src/components/header/Navigation.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 import styled from 'styled-components'
 import NavigationItem, { Props as NavProps } from './NavigationItem'
 import { useStaticQuery, graphql } from 'gatsby'
+import media from '~/utils/media'
 
 export default () => {
   const data = useStaticQuery(query)
 
   return (
-    <Container>
-      {data.site.siteMetadata.navigation.map((nav: NavProps, i: number) => (
-        <NavigationItem key={i} {...nav} />
-      ))}
-    </Container>
+    <>
+      <WideNavContainer>
+        {data.site.siteMetadata.navigation.map((nav: NavProps, i: number) => (
+          <NavigationItem key={i} {...nav} />
+        ))}
+      </WideNavContainer>
+      <NarrowNavContainer>
+        <div>narrow nav</div>
+      </NarrowNavContainer>
+    </>
   )
 }
 
@@ -32,11 +38,16 @@ const query = graphql`
   }
 `
 
-const Container = styled.ul`
+const WideNavContainer = styled.ul`
+  display: none;
+
+  ${media.lg} {
+    display: flex;
+  }
+
   font-family: ${props => props.theme.header.font};
   font-size: 1.05em;
   color: ${props => props.theme.header.color};
-  display: flex;
   height: 100%;
   padding: 0;
   margin: 0;
@@ -46,4 +57,12 @@ const Container = styled.ul`
   justify-content: space-around;
   position: relative;
   z-index: 1000;
+`
+
+const NarrowNavContainer = styled.div`
+  display: block;
+
+  ${media.lg} {
+    display: none;
+  }
 `
