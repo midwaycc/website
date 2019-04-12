7845c12b2559a9a6858623194467e7bf1deef67e
Kevin Smith <ksmithbaylor@gmail.com>
2019-04-11 22:05:38 -0400

    Add animated hamburger/X to mobile nav

--- src/components/header/Navigation/Narrow/index.tsx
+++ src/components/header/Navigation/Narrow/index.tsx
@@ -7,7 +7,16 @@ type Props = {
 }
 
 export default ({ navigationItems }: Props) => {
-  return <NarrowNavContainer>mobile nav</NarrowNavContainer>
+  return (
+    <NarrowNavContainer>
+      <Hamburger>
+        <Toggle />
+        <HamburgerLine />
+        <HamburgerLine />
+        <HamburgerLine />
+      </Hamburger>
+    </NarrowNavContainer>
+  )
 }
 
 const NarrowNavContainer = styled.div`
@@ -15,5 +24,75 @@ const NarrowNavContainer = styled.div`
 
   ${media.lg} {
     display: none;
+  }
+
+  position: relative;
+`
+
+const Toggle = styled.input.attrs({ type: 'checkbox' })`
+  display: block;
+  position: absolute;
+  top: -3px;
+  left: -2px;
+  width: ${props => props.theme.header.height}px;
+  height: ${props => props.theme.header.height}px;
+
+  cursor: pointer;
+
+  opacity: 0;
+  z-index: 2;
+
+  -webkit-touch-callout: none;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+`
+
+const Hamburger = styled.div`
+  height: ${props => props.theme.header.height}px;
+  width: ${props => props.theme.header.height}px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const HamburgerLine = styled.span`
+  display: block;
+  width: 2em;
+  height: 0.2em;
+  position: relative;
+  background-color: ${props => props.theme.header.color};
+  border-radius: 0.2em;
+  z-index: 1;
+
+  transform-origin: 0.3em 0;
+
+  transition: transform 0.5s ease, background 0.5s ease, opacity 0.55s ease,
+    width 0.5s ease;
+
+  & + & {
+    margin-top: 0.3em;
+  }
+
+  &:first-child {
+    transform-origin: 0% 0%;
+  }
+
+  &:nth-last-child(2) {
+    transform-origin: 0% 100%;
+  }
+
+  ${Toggle}:checked ~ & {
+    opacity: 1;
+    transform: rotate(45deg) translate(-2px, -1px);
+    width: 2.28em;
+  }
+
+  ${Toggle}:checked ~ &:nth-last-child(2) {
+    opacity: 0;
+    transform: unset;
+  }
+
+  ${Toggle}:checked ~ &:last-child {
+    transform: rotate(-45deg) translate(-2px, -1px);
   }
 `
