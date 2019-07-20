6f1de17e96437771cbecbc794a31fe83824bb21d
Kevin Smith <ksmithbaylor@gmail.com>
2019-07-20 16:59:56 -0400

    Add ministry pages to sanity

--- content/schemas/schema.js
+++ content/schemas/schema.js
@@ -12,5 +12,34 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    {
+      name: 'ministryPage',
+      title: 'Ministry Page',
+      type: 'document',
+      // TODO: validate fields as required, etc
+      fields: [
+        { name: 'name', type: 'string', title: 'Name' },
+        {
+          name: 'identifier',
+          type: 'slug',
+          title: 'URL Identifier (slug)',
+          description:
+            'This will be the last part of the URL (example: to make this page live at /ministries/men, put "men" in this field).'
+        },
+        {
+          name: 'splashImage',
+          type: 'image',
+          title: 'Splash Image',
+          description: 'The size should be exactly 1900px by 300px.'
+          // TODO: validate image dimensions
+        },
+        {
+          name: 'content',
+          type: 'array',
+          of: [{ type: 'block' }],
+          title: 'Content'
+        }
+      ]
+    }
   ])
 })
