df20753b6aef920e407ddf52b32e3ef11fb2fe85
Kevin Smith <ksmithbaylor@gmail.com>
2019-10-18 09:48:40 -0400

    Use typescript/rollup to compile functions ahead of time

--- functions/sanity-check.ts
+++ functions/sanity-check.ts
@@ -1,9 +1,9 @@
-const fetch = require('node-fetch')
+import fetch from 'node-fetch'
 
 const LATEST_DOCUMENT_QUERY =
   '*[!(_id in path("drafts.**") || _id in path("_.**"))]|order(_updatedAt desc)[0]._updatedAt'
 
-exports.handler = async function(event, context) {
+export async function handler() {
   try {
     const [lastContentUpdate, lastDeploy] = await Promise.all([
       minutesSinceLastContentUpdate(),
@@ -35,7 +35,12 @@ exports.handler = async function(event, context) {
   }
 }
 
-const checkTimestamp = (url, token, name, getTimestamp) => async () => {
+const checkTimestamp = (
+  url: string,
+  token: string | undefined,
+  name: string | undefined,
+  getTimestamp: (data: any) => string
+) => async () => {
   const response = await fetch(url, {
     headers: { Authorization: `Bearer ${token}` }
   })
@@ -48,21 +53,21 @@ const checkTimestamp = (url, token, name, getTimestamp) => async () => {
   const data = await response.json()
   const now = new Date()
   const lastUpdate = new Date(getTimestamp(data))
-  return (now - lastUpdate) / 1000 / 60
+  return (Number(now) - Number(lastUpdate)) / 1000 / 60
 }
 
 const minutesSinceLastContentUpdate = checkTimestamp(
   `https://iq9kxmf9.api.sanity.io/v1/data/query/production?query=${LATEST_DOCUMENT_QUERY}`,
   process.env.SANITY_TOKEN,
   'Sanity',
-  data => data.result
+  (data: { result: string }) => data.result
 )
 
 const minutesSinceLastDeploy = checkTimestamp(
   `https://api.netlify.com/api/v1/sites/c096d1f2-899d-4c18-ad5b-211790e57ad8/deploys?page=1&per_page=1`,
   process.env.NETLIFY_TOKEN,
   'Netlify',
-  data => data[0].created_at
+  (data: Array<{ created_at: string }>) => data[0].created_at
 )
 
 const triggerDeploy = async () => {
