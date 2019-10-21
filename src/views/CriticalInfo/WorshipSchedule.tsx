0c982da4b30c45e754df407bf86c221d638a6764
Kevin Smith <ksmithbaylor@gmail.com>
2019-10-21 09:39:32 -0400

    Change weekly schedule

--- src/views/CriticalInfo/WorshipSchedule.tsx
+++ src/views/CriticalInfo/WorshipSchedule.tsx
@@ -4,9 +4,20 @@ import styled, { css } from 'styled-components'
 import media from '~/utils/media'
 
 export default () => (
-  <Container title="Sunday Schedule">
+  <Container title="Weekly Schedule">
+    <DayHeader>Sunday</DayHeader>
     <WeeklyEvent name="Bible Study" time="9:30 AM" />
     <WeeklyEvent name="Worship Service" time="11:00 AM" />
+    <WeeklyEvent name="AWANA" time="5:00 PM" />
+    <WeeklyEvent name="Youth" time="5:00 PM" />
+    <WeeklyEvent name="Adult Q&A" time="5:15 PM" />
+
+    <DayHeader>Wednesday</DayHeader>
+    <WeeklyEvent name="Prayer Meeting" time="7:00 PM" />
+    <WeeklyEvent name="Youth D-Groups" time="7:00 PM" />
+
+    <DayHeader>Thursday</DayHeader>
+    <WeeklyEvent name="Men's Bible Study" time="6:30 AM" />
   </Container>
 )
 
@@ -25,6 +36,16 @@ const WeeklyEvent = (props: { name: string; time: string }) => (
     <span css="opacity: 0.85">{props.time}</span>
   </div>
 )
+
+const DayHeader = styled.h3`
+  font-weight: lighter;
+  font-size: 22px;
+  opacity: 0.8;
+
+  :first-of-type {
+    margin-top: 0;
+  }
+`
 
 const Container = styled(InfoWidget)`
   margin-top: 0;
