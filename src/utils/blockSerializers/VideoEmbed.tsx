d47be8a1fb4b9d281c3b75340097d144909ccf7c
Kevin Smith <ksmithbaylor@gmail.com>
2019-11-23 15:07:18 -0500

    Remove autoplay and staff picks from New Here

--- src/utils/blockSerializers/VideoEmbed.tsx
+++ src/utils/blockSerializers/VideoEmbed.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import loadable from '@loadable/component'
 import 'plyr/dist/plyr.css'
-// import '@vimeo/player
 
 const Plyr = loadable(() => import('react-plyr'))
 
@@ -12,12 +11,8 @@ type Props = {
 }
 
 export const VideoEmbed = ({ node }: Props) => {
-  if (!node || !node.videoId) {
+  if (!node || !node.videoId || typeof document === 'undefined') {
     return null
-  }
-
-  if (typeof document === 'undefined') {
-    return <VimeoEmbed videoId={node.videoId} />
   }
 
   return <PlyrEmbed videoId={node.videoId} type="vimeo" />
@@ -29,40 +24,22 @@ const PlyrEmbed = ({
 }: {
   videoId: string
   type: 'youtube' | 'vimeo'
-}) => <Plyr type={type} videoId={videoId} autoplay />
+}) => {
+  const plyr = useRef<typeof Plyr & { player: { stop: () => void } }>(null)
 
-const VimeoEmbed = ({ videoId }: { videoId: string }) => (
-  <div css="padding:56.25% 0 0 0;position:relative;">
-    <iframe
-      src={`https://player.vimeo.com/video/${videoId}?autoplay=1&color=2B6667&title=0&byline=0&portrait=0`}
-      css="position:absolute;top:0;left:0;width:100%;height:100%;"
-      frameBorder="0"
-      allow="autoplay; fullscreen"
-      allowFullScreen
-    ></iframe>
-  </div>
-)
-
-// const YouTubeEmbed = ({ videoId }: { videoId: string }) => (
-// <div
-// className="video"
-// style={{
-// position: 'relative',
-// paddingBottom: '56.25%' /* 16:9 */,
-// paddingTop: 25,
-// height: 0
-// }}
-// >
-// <iframe
-// style={{
-// position: 'absolute',
-// top: 0,
-// left: 0,
-// width: '100%',
-// height: '100%'
-// }}
-// src={`https://www.youtube.com/embed/${videoId}?color=white&autoplay=1`}
-// frameBorder="0"
-// />
-// </div>
-// )
+  return (
+    <Plyr
+      ref={plyr}
+      type={type}
+      videoId={videoId}
+      clickToPlay
+      loop={{ active: true }}
+      onSeeked={pos => {
+        if (pos === 0 && plyr.current) {
+          plyr.current.player.stop()
+        }
+      }}
+      onEnd={() => alert('end')}
+    />
+  )
+}
