923d831f0167d25eebd32618b1302041d12f93a2
Kevin Smith <ksmithbaylor@gmail.com>
2019-12-14 20:39:05 -0500

    Only show list, not month grid

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,81 +1,28 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { createGlobalStyle } from 'styled-components'
-import Modal from 'react-modal'
 import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
 import listPlugin from '@fullcalendar/list'
 import googleCalendarPlugin from '@fullcalendar/google-calendar'
-import useWindowSize from '@rooks/use-window-size'
-import media from '~/utils/media'
-import { format, addMinutes } from 'date-fns'
 
 import '@fullcalendar/core/main.css'
-import '@fullcalendar/daygrid/main.css'
 import '@fullcalendar/list/main.css'
 
-Modal.setAppElement(document.body)
-
 export default () => {
-  const [event, setEvent] = useState(null)
-  const [modalOpen, setModalOpen] = useState(false)
-  const { innerWidth } = useWindowSize()
-  if (innerWidth === null) return null
-
-  const isSmall = innerWidth < media.lg.pixelWidth
-
-  const handleEventClick = info => {
-    info.jsEvent.preventDefault()
-    info.jsEvent.stopPropagation()
-    console.log(info.event)
-    setEvent(info.event)
-    setModalOpen(true)
-  }
-
-  const closeModal = () => {
-    setModalOpen(false)
-  }
-
   return (
     <>
       <StyleOverrides />
       <FullCalendar
-        key={String(isSmall)}
-        defaultView={isSmall ? 'listMonth' : 'dayGridMonth'}
-        plugins={[dayGridPlugin, listPlugin, googleCalendarPlugin]}
+        defaultView="listMonth"
+        plugins={[listPlugin, googleCalendarPlugin]}
         googleCalendarApiKey="AIzaSyDhddcpnZvFan-d1e7AOTI3UM6of2QdcOk"
-        eventClick={handleEventClick}
         eventSources={calendarIds.map(googleCalendarId => ({
           googleCalendarId,
-          backgroundColor: '#9fb94b',
-          borderColor: '#9fb94b',
+          backgroundColor: 'white',
+          borderColor: 'white',
+          textColor: '#2b6667',
           url: '#'
         }))}
       />
-      <Modal
-        isOpen={modalOpen}
-        onRequestClose={closeModal}
-        style={{
-          overlay: {
-            zIndex: 5,
-            backgroundColor: 'rgba(255,255,255,0.2)'
-          },
-          content: {
-            zIndex: 6,
-            top: 'calc(50% - 80px)',
-            bottom: 'calc(50% - 80px)',
-            left: 'calc(50% - 150px)',
-            right: 'calc(50% - 150px)',
-            padding: '1em'
-          }
-        }}
-      >
-        {event ? (
-          <>
-            <h4 css="margin-top: 0">{event._def.title}</h4>
-            <p>{format(toEST(event._instance.range.start), 'h:mm a')}</p>
-          </>
-        ) : null}
-      </Modal>
     </>
   )
 }
@@ -98,14 +45,6 @@ const calendarIds = [
 ]
 
 const StyleOverrides = createGlobalStyle`
-  .fc-day-grid-container {
-    background-color: #5aa7a9;
-  }
-
-  .fc-today {
-    color: #099799;
-  }
-
   .fc-unthemed .fc-list-item:hover td {
     background-color: #5aa7a9 !important;
   }
@@ -113,9 +52,13 @@ const StyleOverrides = createGlobalStyle`
   .fc-list-heading span {
     color: #2b6667;
   }
-`
 
-function toEST(date: Date) {
-  const offset = date.getTimezoneOffset()
-  return addMinutes(date, offset)
-}
+  .fc-ltr .fc-list-heading-alt {
+    float: unset !important;
+  }
+
+  .fc-ltr .fc-list-heading-alt:before {
+    content: '-';
+    margin: 0 0.5em;
+  }
+`
