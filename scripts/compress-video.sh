b330b2864a39f62bf09932c2ae6cf56799947715
Kevin Smith <ksmithbaylor@gmail.com>
2019-12-20 03:28:22 -0500

    New video, optimizer script, and rearrange hero

---
+++ scripts/compress-video.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+VIDEO="$1"
+VIDEO_PREFIX=$(echo "$VIDEO" | cut -f 1 -d '.')
+
+ffmpeg -i "$VIDEO" -c:v libx264 -crf 28 -preset veryslow -c:a copy "$VIDEO_PREFIX-compressed.mp4"
+ffmpeg -i "$VIDEO_PREFIX-compressed.mp4" -c:v copy -c:a copy -movflags faststart "$VIDEO_PREFIX-faststart.mp4"
+ffmpeg -i "$VIDEO_PREFIX-faststart.mp4" -c:v copy -an "$VIDEO_PREFIX-silent.mp4"
+
+mv "$VIDEO_PREFIX-silent.mp4" "$VIDEO_PREFIX-optimized.mp4"
+rm "$VIDEO_PREFIX-faststart.mp4"
+rm "$VIDEO_PREFIX-compressed.mp4"
--- src/views/Hero/index.tsx
+++ src/views/Hero/index.tsx
@@ -1,39 +1,58 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import styled from 'styled-components'
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import styled, { css } from 'styled-components'
 import media from '~/utils/media'
 import { HeroSectionQuery } from '~/types/graphqlTypes'
+import heroVideo from '~/../static/hero-optimized.mp4'
+import { SquareButton } from '~/components/SquareButton'
 
 export default () => {
   const data: HeroSectionQuery = useStaticQuery(query)
   if (!data.sanityHeroSection) return null
-  const { title, subtitle, video /*, poster */ } = data.sanityHeroSection
+  const { title, subtitle } = data.sanityHeroSection
 
   return (
     <Container>
-      {video && video.asset && video.asset.url && (
-        <VideoContainer>
-          <video
-            autoPlay
-            loop
-            preload=""
-            muted
-            playsInline
-            poster={
-              undefined
-              //poster && poster.asset && poster.asset.url
-              // ? poster.asset.url
-              // : undefined
-            }
-          >
-            <source src={video.asset.url} type="video/mp4" />
-          </video>
-        </VideoContainer>
-      )}
+      <VideoContainer>
+        <video
+          autoPlay
+          loop
+          preload=""
+          muted
+          playsInline
+          poster={
+            undefined
+            //poster && poster.asset && poster.asset.url
+            // ? poster.asset.url
+            // : undefined
+          }
+        >
+          <source src={heroVideo} type="video/mp4" />
+        </video>
+      </VideoContainer>
+
+      {/* <Faders /> */}
 
       <OverlayContainer>
-        <Title>{title}</Title>
-        <Subtitle>{subtitle}</Subtitle>
+        <HeroContent>
+          <Title>{title}</Title>
+          <Subtitle>{subtitle}</Subtitle>
+          <Link to="/new">
+            <SquareButton
+              dark
+              thick
+              css={css`
+                margin-top: 2em;
+                background-color: rgba(255, 255, 255, 0.5);
+                :hover {
+                  background-color: white;
+                }
+              `}
+            >
+              New Here?
+            </SquareButton>
+          </Link>
+        </HeroContent>
       </OverlayContainer>
     </Container>
   )
@@ -44,24 +63,14 @@ const query = graphql`
     sanityHeroSection {
       subtitle
       title
-      video {
-        asset {
-          url
-        }
-      }
-      poster {
-        asset {
-          url
-        }
-      }
     }
   }
 `
 
 const Container = styled.section`
   width: 100%;
-  height: calc(100vh - ${props => props.theme.header.height}px - 7em);
-  min-height: 480px;
+  height: calc((100vw / 16) * 9);
+  min-height: 720px;
   overflow: hidden;
   position: relative;
 `
@@ -79,25 +88,61 @@ const VideoContainer = styled.div`
   }
 `
 
+const FADE_OVERLAP = 400
+const Faders = styled.div`
+  :before,
+  :after {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: calc(100vw/2 - 88.8889vh + ${props =>
+      props.theme.header.height * 0.888889}px + ${FADE_OVERLAP}px);
+    /* width: calc(
+      100vw - ((100vh - ${props => props.theme.header.height}px) * 0.88888889)
+    ); */
+  }
+
+  :before {
+    left: 0;
+    background-image: linear-gradient(to right, rgb(10, 76, 7) 0%, rgb(10, 76, 7) calc(100% - ${FADE_OVERLAP -
+      10}px), rgba(0,0,0,0) 100%);
+  }
+
+  :after {
+    right: 0;
+    background-image: linear-gradient(to left, rgb(115, 134, 55) 0%, rgb(115, 134, 55) calc(100% - ${FADE_OVERLAP -
+      10}px), rgba(0,0,0,0) 100%);
+  }
+`
+
 const OverlayContainer = styled.div`
   width: 100%;
   height: 100%;
-  margin: 0 auto;
-  display: flex;
   position: absolute;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  position: relative;
   color: ${props => props.theme.hero.color};
   background-image: linear-gradient(
     90deg,
-    rgba(10, 76, 77, 0.4) 0%,
-    rgba(115, 134, 55, 0.4) 130%
+    rgba(10, 76, 77, 0.5) 0%,
+    rgba(115, 134, 55, 0.5) 130%
   );
 
   & > * {
     max-width: ${media.xl.width};
+  }
+`
+
+const HeroContent = styled.div`
+  width: 100%;
+  /* height: calc(100vh - ${props => props.theme.header.height}px); */
+  text-align: center;
+  margin: 0 auto;
+
+  :before {
+    content: '';
+    display: block;
+    max-height: 10em;
+    height: calc(20vh);
   }
 `
 
@@ -117,10 +162,9 @@ const Title = styled.h1`
 `
 
 const Subtitle = styled.p`
-  margin: 2em 0;
-  max-width: calc(100% - 4em - 60px);
-  text-align: center;
-  /* font-size: 1.2em; */
+  max-width: calc(100vw - 4em);
+  margin: 1em auto;
+  font-size: 1.4em;
 
   ${media.sm} {
     font-size: 1.25em;
