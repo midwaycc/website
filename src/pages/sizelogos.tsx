e21c769805b9e5a06e4a327ae7450e015eb636ae
Kevin Smith <ksmithbaylor@gmail.com>
2020-01-20 14:08:07 -0500

    Allow sizelogos page to skip ministry pages without logos

--- src/pages/sizelogos.tsx
+++ src/pages/sizelogos.tsx
@@ -8,14 +8,16 @@ export default ({ data }: { data: any }) => {
 
   return (
     <Section css="padding: 2em 1.5em">
-      {ministries.map((ministry: any) => (
-        <SubLogo
-          key={ministry.name}
-          width={ministry.subLogoWidth}
-          fluid={ministry.subLogo.asset.fluid}
-          css="margin-bottom: 2em"
-        />
-      ))}
+      {ministries.map((ministry: any) =>
+        ministry && ministry.subLogo && ministry.subLogo.asset ? (
+          <SubLogo
+            key={ministry.name}
+            width={ministry.subLogoWidth}
+            fluid={ministry.subLogo.asset.fluid}
+            css="margin-bottom: 2em"
+          />
+        ) : null
+      )}
     </Section>
   )
 }
