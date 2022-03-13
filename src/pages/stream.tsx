162f1e0b0dc75d459a0973492b750a8bb56ee241
Kevin Smith <ksmithbaylor@gmail.com>
2022-03-13 08:55:50 -0400

    Use production streaming links

--- src/pages/stream.tsx
+++ src/pages/stream.tsx
@@ -16,7 +16,7 @@ export default function StreamPage() {
           <iframe
             allow="autoplay; fullscreen"
             allowFullScreen
-            src="https://control.resi.io/webplayer/video.html?id=38d02044-bbd6-4bb3-9f54-4aca233339cb"
+            src="https://control.resi.io/webplayer/video.html?id=8da28110-49b5-4486-90a7-c4b71457e86f"
             style={{
               position: 'absolute',
               top: 0,
@@ -28,7 +28,7 @@ export default function StreamPage() {
           ></iframe>
         </div>
         <a
-          href="https://control.resi.io/webplayer/video.html?id=38d02044-bbd6-4bb3-9f54-4aca233339cb"
+          href="https://control.resi.io/webplayer/video.html?id=8da28110-49b5-4486-90a7-c4b71457e86f"
           target="_blank"
           rel="noopener noreferrer"
           style={{ display: 'block', paddingTop: '1rem', textAlign: 'center' }}
