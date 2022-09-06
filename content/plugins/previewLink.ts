a3144efb52e30b328b121af65d2f152324ec43a4
Kevin Smith <ksmithbaylor@gmail.com>
2022-09-06 13:30:35 -0400

    Upgrade sanity preview

--- content/plugins/previewLink.ts
+++ content/plugins/previewLink.ts
@@ -1,46 +1,6 @@
-type Doc = {
-  _type: string
-  url?: {
-    current: string
-  }
-  slug?: {
-    current: string
-  }
-}
+import defaultResolve from 'part:@sanity/base/document-actions'
+import { gatsbyPreviewAction } from 'sanity-plugin-gatsby-cloud-preview'
 
-export default function resolveProductionUrl(document: Doc) {
-  const dataset = window.location.pathname.split('/')[1] as
-    | 'production'
-    | 'staging'
-
-  if (dataset === 'production') {
-    return `https://preview-midwaycc.gtsb.io${path(document)}`
-  }
-
-  return `https://preview-midwaycc.gtsb.io${path(document)}`
-}
-
-function path(document: Doc) {
-  switch (document._type) {
-    case 'page':
-    case 'ministryPage':
-      return document.url?.current
-    case 'heroSection':
-    case 'navigation':
-    case 'shortcuts':
-    case 'generalAlert':
-    case 'scheduleAlert':
-    case 'weeklySchedule':
-    case 'poster':
-      return '/'
-    case 'post':
-      return `/posts/${document.slug?.current}`
-    case 'newsletter':
-      return '/newsletter'
-    case 'sermonUpload':
-      return '/sermons'
-    default:
-      alert('Kevin needs to set up a preview link for this one!')
-      return '/'
-  }
+export default function resolveDocumentActions(props: unknown) {
+  return [...defaultResolve(props), gatsbyPreviewAction]
 }
